2f3fe8d96716a474bebe125009f52e5f2f367e5d
Shachar Ron Zohar <Rzshachar@gmail.com>
2022-02-09 16:12:51 +0200

    Added onSubmitForm, which takes the values from the form and saves it to storage as a string, aswell as renders the relevant values

--- js/placeKeeperController.js
+++ js/placeKeeperController.js
@@ -6,12 +6,25 @@ function onInit() {
     })
 }
 
-function onSubmitForm(ev) {
-    console.log('ev', ev)
-
-
+function onSubmitForm() {
+    const vals = {
+        email: document.querySelector('#email').value,
+        age: document.querySelector('#age').value,
+        'bgColor': document.querySelector('#bg-color').value,
+        'txtColor': document.querySelector('#txt-color').value,
+        'birthDate': document.querySelector('#birth-date').value,
+        'birthTime': document.querySelector('#birth-time').value,
+    }
+    _renderUserColors(vals)
+    onSaveToStorage(vals)
 }
 
 function showAge(newVal) {
     document.getElementById("sAge").innerHTML = newVal;
+}
+
+function _renderUserColors({ bgColor, txtColor }) {
+    document.querySelector('body').style.backgroundColor = bgColor
+    document.querySelector('body').style.color = txtColor
+
 }
